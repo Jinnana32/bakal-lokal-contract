19e37ba362536d281db1ba23720084169ad10936
tjcoyoca <tjcoyoca17@gmail.com>
2022-09-25 15:22:28 +0800

    Updated contract script and blockchain script

--- src/blockchain/index.ts
+++ src/blockchain/index.ts
@@ -1,34 +1,36 @@
-import { ethers, providers, BytesLike } from "ethers";
-import contractABI from '../../abi.json';
+import { ethers, BytesLike } from "ethers";
+import contractABI from "../../abi.json";
 
 export let smartContract: ethers.Contract;
+export let signer: ethers.Wallet;
 
-export function initChain(privKey: BytesLike, contractAddress: string){
-    if(smartContract) {
-        return;
-    }
+export async function init(privKey: BytesLike, contractAddress: string) {
+  if (smartContract) {
+    return;
+  }
 
-    const bscNetwork = `https://data-seed-prebsc-1-s1.binance.org:8545/`;
-    const signer = new ethers.Wallet(
-        privKey,
-        providers.getDefaultProvider(bscNetwork)
-    );
-    
-    smartContract = new ethers.Contract(contractAddress, contractABI, signer);
+  const bscProvider = new ethers.providers.JsonRpcProvider(
+    "https://bsc-dataseed.binance.org/",
+    { name: "binance", chainId: 56 }
+  );
+  signer = new ethers.Wallet(privKey, bscProvider);
+  smartContract = new ethers.Contract(contractAddress, contractABI, signer);
 }
 
 export async function addNewDailyRecord(jsonString: string) {
-    const buffer = Buffer.from(jsonString);
-    const record = buffer.toString('base64');
-    await smartContract.functions.addRecord(record);
+  const buffer = Buffer.from(jsonString);
+  const record = buffer.toString("base64");
+  await smartContract.functions.addRecord(record);
 }
 
-export async function getRecordsLength() : Promise<number> {
-    let result = await smartContract.functions.getRecordLength();
-    return result[0];
+export async function getRecordsLength(): Promise<number> {
+  let result = await smartContract.functions.getRecordLength();
+  return result[0];
 }
 
-export async function getRecordByIndex(index: number) : Promise<[number, string]> {
-    const results = await smartContract.functions.getRecord(index);
-    return [results[0], results[1]];
+export async function getRecordByIndex(
+  index: number
+): Promise<[number, string]> {
+  const results = await smartContract.functions.getRecord(index);
+  return [results[0], results[1]];
 }
